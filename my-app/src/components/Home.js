e2967d5a66f98b19fc585913f47c8c8df6c97f9f
riley <rilesdou@gmail.com>
2023-04-12 19:30:13 -0400

    add homepage

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import vid from '../assets/video/train.mov'
 
 export default function Home() {
 
@@ -19,18 +20,25 @@ export default function Home() {
     return (
         <>
             <div className="cont">
-                <div>
-                    <h2 className="company" style={{fontFamily: "nanum", fontSize: 50}}>Commuter Clock</h2>
-                    <h4 className="company" style={{fontFamily: "nanum", fontSize: 25}}>by City Surfers Inc.</h4>
+                <div className="header">
+                    <div>
+                        <h2 className="company" style={{fontFamily: "nanum", fontSize: 50}}>Commuter Clock</h2>
+                        <h4 className="company" style={{fontFamily: "nanum", fontSize: 25}}>by City Surfers Inc.</h4>
+                    </div>
+                    <div>
+                        <Button className="login" variant="dark" onClick={login}>Login</Button>
+                        <Button variant="dark" onClick={signup}>Sign Up</Button>
+                    </div>
                 </div>
-                <div className="buttons">
-                    <Button className="login" variant="dark" onClick={login}>Login</Button>
-                    <Button variant="dark" onClick={signup}>Sign Up</Button>
+                <h1 className="about">
+                    About Us
+                </h1>
+                <div className="video">
+                    <video style={{display: 'flex', justifyContent: 'center', alignItems: 'center', maxWidth: '600px'}}autoPlay loop muted>
+                            <source src={vid}></source>
+                    </video>
                 </div>
             </div>
-            <video id="background-vid" autoPlay loop muted>
-                
-            </video>
         </>
     )
 }

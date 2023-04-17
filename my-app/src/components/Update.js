fcb40b09c4e1f4fc56123e767c02d9489b2f92ea
riley <rilesdou@gmail.com>
2023-04-17 12:48:12 -0400

    change update

--- my-app/src/components/Update.js
+++ my-app/src/components/Update.js
@@ -6,9 +6,6 @@ import app from '../firebase.js'
 
 export default function Edit() {
 
-    const db = getFirestore(app);
-    var data = [];
-
     const userRef = useRef()
     const startPtRef = useRef()
     const destRef = useRef()
@@ -16,30 +13,6 @@ export default function Edit() {
     const endTimeRef = useRef()
     const waitTimeRef = useRef()
     const carRef = useRef()
-
-    const [startPt, setStartPt] = useState("")
-    const [dest, setDest] = useState("")
-    const [startTime, setStartTime] = useState("")
-    const [endTime, setEndTime] = useState("")
-    const [waitTime, setWaitTime] = useState("")
-    const [wantCar, setWantCar] = useState(false)
-
-    getDoc(doc(db, "settings", "user")).then(docSnap => {
-        if (docSnap.exists()) {
-            data = Object.values(docSnap.data())
-            console.log(data);
-            
-            setStartPt(data[1])
-            setDest(data[2])
-            setStartTime(data[0])
-            setEndTime(data[3])
-            setWaitTime(data[4])
-            setWantCar(data[5])
-    
-        } else {
-            console.log("No such document!");
-        }
-        })
 
     function handleSubmit(e){
 
@@ -58,11 +31,6 @@ export default function Edit() {
 
     }
 
-    function handleChange(e){
-        e.preventDefault()
-
-    }
-
     return (
         <>
         <h1 className="text-center mb-4" id="header">Update</h1>
@@ -71,31 +39,31 @@ export default function Edit() {
                     <Form id="form" onSubmit={handleSubmit}> 
                         <Form.Group id="user">
                             <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" ref={userRef} onChange={handleChange}></Form.Control>
+                            <Form.Control type="text" ref={userRef}></Form.Control>
                         </Form.Group>
                         <Form.Group id="start">
                             <Form.Label>Starting Point</Form.Label>
-                            <Form.Control type="text" ref={startPtRef} onChange={handleChange} required></Form.Control>
+                            <Form.Control type="text" ref={startPtRef} required></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Destination</Form.Label>
-                            <Form.Control id="end" type="text" ref={destRef} onChange={handleChange}required></Form.Control>
+                            <Form.Control id="end" type="text" ref={destRef} required></Form.Control>
                         </Form.Group>
                         <Form.Group >
                             <Form.Label>Start Time</Form.Label>
-                            <Form.Control id="start_time" type="time" ref={startTimeRef} onChange={handleChange} required></Form.Control>
+                            <Form.Control id="start_time" type="time" ref={startTimeRef} required></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>End Time</Form.Label>
-                            <Form.Control id="end_time" type="time" ref={endTimeRef} onChange={handleChange} required></Form.Control>
+                            <Form.Control id="end_time" type="time" ref={endTimeRef} required></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Wait Time</Form.Label>
-                            <Form.Control id="wait_time" type="number" ref={waitTimeRef} onChange={handleChange} required></Form.Control>
+                            <Form.Control id="wait_time" type="number" ref={waitTimeRef} required></Form.Control>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Car</Form.Label>
-                            <Form.Check type="switch" onChange={handleChange}></Form.Check>
+                            <Form.Check type="switch"></Form.Check>
                         </Form.Group>
                         <br></br>
                         <Button className="w-100" type="submit" border="dark" variant="dark">Update</Button>
